00a7cc704cad5f89e6a78e07910b8da5da38b76c
wheatgrinder <wheatgrinder@yahoo.com>
2018-01-16 18:08:58 -0800

    supports RGB LEDS and pan_tilt controller

--- d2/config.py
+++ d2/config.py
@@ -44,6 +44,41 @@ THROTTLE_FORWARD_PWM =480
 THROTTLE_STOPPED_PWM = 410 
 THROTTLE_REVERSE_PWM = 310 
 
+
+# neck 
+# neck tilt
+# channel = 3
+TILT_CHANNEL = 3
+TILT_UP_PWM = 150
+TILT_DOWN_PWM = 525
+TILT_CENTER_PWM = 260
+TILT_DRIVING_PWM = 320
+
+# tilt down max = 525
+# tilt down looking straight down at ground 525 clears platrom etc..
+# tilt level looking direct forward  260
+
+
+# neck pan
+# channel = 2
+PAN_CHANNEL = 2
+PAN_RIGHT_PWM = 85
+PAN_LEFT_PWM = 600
+PAN_CENTER_PWM = 325
+# pan max right 60
+# pan max right 100 clears all
+# pan max left 520..clears all 
+# pan max left 570
+# center forward 295
+#  
+
+TAIL_CHANNEL = 15
+TAIL_UP_PWM = 300
+TAIL_DOWN_PWM = 100
+TAIL_CENTER_PWM = 200
+
+
+
 #TRAINING
 BATCH_SIZE = 128
 TRAIN_TEST_SPLIT = 0.8
@@ -53,4 +88,4 @@ TRAIN_TEST_SPLIT = 0.8
 USE_JOYSTICK_AS_DEFAULT = False
 JOYSTICK_MAX_THROTTLE = 1.0
 JOYSTICK_STEERING_SCALE = 1.0
-AUTO_RECORD_ON_THROTTLE = True
+AUTO_RECORD_ON_THROTTLE = False
--- d2/manage.py
+++ d2/manage.py
@@ -16,35 +16,40 @@ from docopt import docopt
  
 import donkeycar as dk
 
-
+ 
 #import parts
 from donkeycar.parts.camera import PiCamera
 #from donkeycar.parts.cv import ImgGreyscale
 #from donkeycar.parts.cv import ImgCanny
-from donkeycar.parts.cv import ImgCrop
-from donkeycar.parts.cv import ImgDrawLine
-from donkeycar.parts.cv import ImgPutInfo
+#from donkeycar.parts.cv import ImgCrop
+#from donkeycar.parts.cv import ImgDrawLine
+#from donkeycar.parts.cv import ImgPutInfo
 from donkeycar.parts.cv import ImgPutText
 from donkeycar.parts.cv import ImgResize
+from donkeycar.parts.cv import ImgBoostBright
 from donkeycar.parts.transform import Lambda
 from donkeycar.parts.keras import KerasCroppedCategorical
 from donkeycar.parts.actuator import PCA9685, PWMSteering, PWMThrottle
+ 
+# use only one PCA9685 controller. The pan tilt needs the controller but can use the one setup by the acturator part
+from donkeycar.parts.pan_tilt_cam import PWMPanning, PWMTilting, PWMWagging
+
 from donkeycar.parts.datastore import TubHandler, TubGroup
-from donkeycar.parts.controller import LocalWebController, JoystickController
+#from donkeycar.parts.controller import LocalWebController, JoystickController
+from donkeycar.parts.controller_with_pan_and_tilt import LocalWebController, JoystickController
 
 #NCS PARTS
 from donkeycar.parts.ncs import googlenet
 from donkeycar.parts.ncs import inception
 from donkeycar.parts.ncs import tinyyolo
 
-from donkeycar.parts.govenor import break_for
-from donkeycar.parts.pitft import display_text
-
-
-
+#from donkeycar.parts.govenor import break_for
+#from donkeycar.parts.pitft import display_text
+from donkeycar.parts.PCA9685_LED import rgb_led, turn_signal, status_indicator
+#
 from donkeycar.parts.perfmon import driveLoopTime
-from donkeycar.parts.perfmon import coreTemp
-from donkeycar.parts.perfmon import throttled
+#from donkeycar.parts.perfmon import coreTemp
+#from donkeycar.parts.perfmon import throttled
 
 def drive(cfg, model_path=None, use_joystick=False):
     '''
@@ -55,30 +60,45 @@ def drive(cfg, model_path=None, use_joystick=False):
     cfg.DRIVE_LOOP_HZ assuming each part finishes processing in a timely manner.
     Parts may have named outputs and inputs. The framework handles passing named outputs
     to parts requesting the same named input.
-    ''' 
+    '''   
     
     #Initialize car
     V = dk.vehicle.Vehicle()
+    
+    #add led part
+    #right_led = rgb_led_0(red_channel = 5, green_channel = 6, blue_channel = 7) 
+    center_led = rgb_led(red_channel = 9, green_channel = 10, blue_channel = 11)  
+    #left_led = rgb_led_0(red_channel = 13, green_channel = 14, blue_channel = 15)  
+    #turn_signals = turn_signal(left_led = left_led, right_led = right_led)
+    
+    status_led = status_indicator(status_led = center_led)  
+    
+    #V.add(right_led, outputs=['none'])
+    V.add(center_led, outputs=['none'])
+    #V.add(left_led, outputs=['none'])
+    
+    
     #add pi_perfchecker
     loop_time = driveLoopTime()
-    loop_time.console=False
+    #loop_time.console=True
     #core_temp = coreTemp()
     
-    #V.add(loop_time,inputs = ['timein'], outputs = ['timein','displaytext'])
+    V.add(loop_time,inputs = ['timein'], outputs = ['timein','displaytext'])
     #V.add(core_temp)
-    throtled_status = throttled()
-    V.add(throtled_status, outputs=['displaytext'],threaded=True)
-    pitft=display_text()
-    V.add(pitft, inputs=['displaytext'], outputs=['pitft/screen'], threaded=True)
+    #throtled_status = throttled()
+    #V.add(throtled_status, outputs=['displaytext'],threaded=True)
+    #pitft=display_text()
+    #V.add(pitft, inputs=['displaytext'], outputs=['pitft/screen'], threaded=True)
 
-
+    
     cam = PiCamera(resolution=cfg.CAMERA_RESOLUTION)
     V.add(cam, outputs=['cam/image_array'], threaded=True)
     
-    
-     #ncs_gn = googlenet(basedir=cfg.MODELS_PATH, debug=True)
+    #boostBright=ImgBoostBright()
+    #V.add(boostBright, inputs=['cam/image_array'], outputs=['cam/image_array'])
+    #ncs_gn = googlenet(basedir=cfg.MODELS_PATH, debug=True)
     #V.add(ncs_gn, inputs=['cam/image_array'],outputs=['ncs/image_array', 'classificaiton'],threaded=True)
-    
+     
     #ncs_inception = inception(basedir=cfg.MODELS_PATH, probability_threshold=0.01, debug=True)
     #V.add(ncs_inception, inputs=['cam/image_array'],outputs=['ncs/image_array', 'classificaiton'],threaded=True)
     
@@ -121,28 +141,37 @@ def drive(cfg, model_path=None, use_joystick=False):
 
     if use_joystick or cfg.USE_JOYSTICK_AS_DEFAULT:
         #modify max_throttle closer to 1.0 to have more power
-        #modify steering_scale lower than 1.0 to have less responsive steering
+        #modify steering_scale lower than 1.0 to have less responsive steering 
+         # auto_record_on_throttle=cfg.AUTO_RECORD_ON_THROTTLE,throttle_axis='rz',steering_axis='x')
         ctr = JoystickController(max_throttle=cfg.JOYSTICK_MAX_THROTTLE,
                                  steering_scale=cfg.JOYSTICK_STEERING_SCALE,
-                                 auto_record_on_throttle=cfg.AUTO_RECORD_ON_THROTTLE,throttle_axis='rz',steering_axis='x')
+                                 auto_record_on_throttle=cfg.AUTO_RECORD_ON_THROTTLE,
+                                 throttle_axis='y',
+                                 steering_axis='x',
+                                 panning_axis='z',
+                                 tilting_axis='rz')
 
         ctr_webview = LocalWebController()
         V.add(ctr_webview,          
+                #inputs=['ncs/image_array'], 
                 inputs=['ncs/image_array'], 
                 outputs=['user/angle', 'user/throttle', 'user/mode', 'recording'], 
                 threaded=True)
 
     else:        
         #This web controller will create a web server that is capable
-        #of managing steering, throttle, and modes, and more.
+        #of managing steering, throttle, and modes, and more. 
         ctr = LocalWebController()
         #ctr.auto_record_on_throttle = False
     
     V.add(ctr,          
           inputs=['ncs/image_array'],
-          outputs=['user/angle', 'user/throttle', 'user/mode', 'recording'],
+          outputs=['user/angle', 'user/throttle', 'user/mode', 'recording','user/pan','user/tilt'],
           threaded=True)
     
+    # add the LED controller part
+    
+
     #See if we should even run the pilot module. 
     #This is only needed because the part run_condition only accepts boolean
     def pilot_condition(mode):
@@ -156,7 +185,10 @@ def drive(cfg, model_path=None, use_joystick=False):
     
 
     #Run the pilot if the mode is not user.
+    #kl = KerasCroppedCategorical()
+    # Reverting to non cropped.. adding pan\tilt to camerra 
     kl = KerasCroppedCategorical()
+    
     if model_path:
         kl.load(model_path)
     
@@ -176,11 +208,11 @@ def drive(cfg, model_path=None, use_joystick=False):
             return user_angle, user_throttle
         
         elif mode == 'local_angle':
-            #return pilot_angle, user_throttle
-            return pilot_angle, 0.30
+            return pilot_angle, user_throttle
+            #return pilot_angle, 0.30
         else: 
             #overite throttle
-            #return pilot_angle, pilot_throttle
+            #return pilot_angle, pilot_throttle 
             #return pilot_angle, 0.80
             return pilot_angle, pilot_throttle 
     
@@ -206,14 +238,43 @@ def drive(cfg, model_path=None, use_joystick=False):
                                     zero_pulse=cfg.THROTTLE_STOPPED_PWM, 
                                     min_pulse=cfg.THROTTLE_REVERSE_PWM)
 
+
+    panning_controller = PCA9685(cfg.PAN_CHANNEL)
+    panning = PWMPanning(controller=panning_controller,
+                                    left_pulse=cfg.PAN_LEFT_PWM, 
+                                    zero_pulse=cfg.PAN_CENTER_PWM,
+                                    right_pulse=cfg.PAN_RIGHT_PWM)
+
+    tilting_controller = PCA9685(cfg.TILT_CHANNEL)
+    tilting = PWMThrottle(controller=tilting_controller,
+                                    max_pulse=cfg.TILT_UP_PWM,
+                                    zero_pulse=cfg.TILT_DRIVING_PWM, 
+                                    min_pulse=cfg.TILT_DOWN_PWM)
+
+    wagging_controller = PCA9685(cfg.TAIL_CHANNEL)
+    wagging = PWMThrottle(controller=wagging_controller,
+                                    max_pulse=cfg.TAIL_UP_PWM,
+                                    zero_pulse=cfg.TAIL_CENTER_PWM, 
+                                    min_pulse=cfg.TAIL_DOWN_PWM)
+
     #throttleText.text = throttle
-        # add govenor part here.  Governer has overridding power when drive mode is pilot
+         # add govenor part here.  Governer has overridding power when drive mode is pilot
     #break_for_dog = break_for('dog', .3)
     #V.add(break_for_dog, inputs=['user/mode','angle', 'throttle','ncs/found_objs'], outputs=['angle','throttle'])
-    
+        
+    #V.add(turn_signals, inputs=['angle']) 
+    #'user/angle', 'user/throttle', 'user/mode', 'recording'
+    V.add(status_led, inputs=['user/mode', 'recording'])    
+ 
 
     V.add(steering, inputs=['angle'])
     V.add(throttle, inputs=['throttle'])
+    
+    V.add(panning, inputs=['user/pan'])
+    V.add(tilting, inputs=['user/tilt'])
+    #V.add(wagging, inputs=['throttle'])
+
+    
     
     #add tub to save data
     inputs=['cam/image_array', 'user/angle', 'user/throttle', 'user/mode']
@@ -222,6 +283,8 @@ def drive(cfg, model_path=None, use_joystick=False):
     th = TubHandler(path=cfg.DATA_PATH)
     tub = th.new_tub_writer(inputs=inputs, types=types)
     V.add(tub, inputs=inputs, run_condition='recording')
+
+    
     
     #run the vehicle for 20 seconds
     V.start(rate_hz=cfg.DRIVE_LOOP_HZ, 
